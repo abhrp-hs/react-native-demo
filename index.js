82ee74bcb53882682244c033e24e9fd0317fbd72
abhrp-hs <abhiroop@hopscotch.in>
2017-08-18 18:23:13 +0530

    Added few views

--- index.js
+++ index.js
@@ -9,24 +9,36 @@ import {
   AppRegistry,
   StyleSheet,
   Text,
-  View
+  View,
+  Image,
+  StatusBar,
+  ScrollView
 } from 'react-native';
 
 export default class FirstDemo extends Component {
   render() {
+    let pic = {uri: 'https://media4.popsugar-assets.com/files/2015/06/01/782/n/1922398/af0f7c22_Come-at-me-bro.xxxlarge.gif'};
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
-      </View>
+      <ScrollView>
+        <Text style={styles.bodyText}>Hello World</Text>
+        <Text style={styles.bodyText}>Look what i found</Text>
+        <Text style={styles.bodyText}>Tan tan tana tan tan tana tan tan...</Text>
+        <Image source={pic} style={{width: 500, height: 400}}/>
+        <Westeros name='Stark'></Westeros>
+        <Westeros name="Targaryen"></Westeros>
+        <Westeros name="Lannister"></Westeros>
+        <Westeros name="Baratheon"></Westeros>
+        <Westeros name="Greyjoy"></Westeros>
+      </ScrollView>
+    );
+  }
+}
+
+// Creating custom components
+class Westeros extends Component {
+  render() {
+    return (
+      <Text style={styles.bigText}>House {this.props.name}</Text>
     );
   }
 }
@@ -34,20 +46,30 @@ export default class FirstDemo extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    backgroundColor: '#00000000',
   },
   welcome: {
-    fontSize: 20,
+    fontSize: 30,
     textAlign: 'center',
     margin: 10,
+  },
+  bodyText: {
+    fontSize: 20,
+    textAlign: 'left',
+    margin: 5
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
   },
+  bigText: {
+    fontSize: 30,
+    textAlign: 'center',
+    margin: 10
+  }
 });
 
 AppRegistry.registerComponent('FirstDemo', () => FirstDemo);
