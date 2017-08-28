e8cb0a5ac25c517dc24ba4f5a49ffb1816ae372f
Anil Chahal <anilc@hopscotch.in>
2017-08-28 11:38:23 +0530

    Login Module Added

---
+++ login.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import { AppRegistry, StyleSheet, Text, View, Image } from "react-native";
+
+import Main from "./src/components/Main";
+
+export default class FirstDemo extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Main />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5FCFF"
+  }
+});
+
+AppRegistry.registerComponent("FirstDemo", () => FirstDemo);
--- toolbar.js
+++ toolbar.js
@@ -1,12 +1,29 @@
 import React, { Component } from "react";
-import { ActivityIndicator, ListView, Text, View, AppRegistry } from "react-native";
+import {
+  ActivityIndicator,
+  ListView,
+  Text,
+  View,
+  ScrollView,
+  StyleSheet,
+  TextInput,
+  ToolbarAndroid,
+  DatePickerAndroid,
+  Button,
+  Alert,
+  AppRegistry
+} from "react-native";
 
 export default class FirstDemo extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      moviesResponse: [],
       isLoading: true
     };
+    this.dataSource = new ListView.DataSource({
+      rowHasChanged: (r1, r2) => r1 != r2
+    });
   }
 
   componentDidMount() {
@@ -16,6 +33,11 @@ export default class FirstDemo extends Component {
         let ds = new ListView.DataSource({
           rowHasChanged: (r1, r2) => r1 !== r2
         });
+
+        moviesResponse = [];
+        moviesResponse.push(responseJson.movies[0].title);
+        console.log(moviesResponse);
+
         this.setState(
           {
             isLoading: false,
@@ -31,27 +53,89 @@ export default class FirstDemo extends Component {
       });
   }
 
+  onPressDatePicker() {
+    try {
+      const { action, year, month, day } = DatePickerAndroid.open({
+        date: new Date(2017, 8, 20)
+      });
+      if (action !== DatePickerAndroid.dismissedAction) {
+        // Selected year and month and day.
+      }
+    } catch ({ code, message }) {
+      //console.warn("Can not open date picker", message);
+    }
+  }
+
   render() {
     if (this.state.isLoading) {
       return (
-        <View style={{ flex: 1, paddingTop: 20 }}>
+        <View style={styles.alignItemCenter}>
+          <Text>It's Loading... Please Wait</Text>
           <ActivityIndicator />
         </View>
       );
     }
 
     return (
-      <View style={{ flex: 1, paddingTop: 20 }}>
+      <View>
+        <ToolbarAndroid
+          style={styles.toolbar}
+          title="Movies List"
+          actions={[{ title: "Logout", show: "never" }]}
+        />
         <ListView
           dataSource={this.state.dataSource}
-          renderRow={rowData =>
-            <Text>
-              {rowData.title}, {rowData.releaseYear}
-            </Text>}
+          renderRow={rowData => <MoviesList name={rowData.title} />}
         />
+        <Button title="Date Picker" onPress={this.onPressDatePicker} />
       </View>
     );
   }
 }
+
+// creating custom component
+class MoviesList extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          borderBottomColor: "black",
+          borderBottomWidth: 0
+        }}
+      >
+        <Text style={styles.bodyText}>
+          {" "}Hello {this.props.name}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bodyText: {
+    fontSize: 20,
+    textAlign: "center",
+    margin: 10
+  },
+  toolbar: {
+    backgroundColor: "#2196F3",
+    height: 56,
+    textAlign: "center"
+  },
+  titleCenter: {
+    flex: 1,
+    textAlign: "center",
+    justifyContent: "center"
+  },
+  alignItemCenter: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 AppRegistry.registerComponent("FirstDemo", () => FirstDemo);
